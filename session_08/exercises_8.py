d142db38a8b3abb4fc0687557d8960bcfe158cc2
Replit user <>
2023-09-14 15:31:36 +0000

    session 8

--- session_08/exercises_8.py
+++ session_08/exercises_8.py
@@ -54,11 +54,53 @@ f.close()
 
 
 # 2. Create a new file called 'even.txt' that contains only the even numbers from the file 'numbers.txt'.
+f = open("text_files/even.txt", "w")
 
+for x in open("text_files/numbers.txt"):
+    x = int(x)
+    if x % 2 == 0:
+        f.write(str(x) + "\n")
+
+f.close()
 
 
 # 3. 'secret.txt' contains a secret message. Each number represents the letter of the alphabet where 1 = A, 2 = B ... Z = 26. 
 #    Work out what the secret message says.
+dictionary = {
+    0: "!",
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H",
+    9: "I",
+    10: "J",
+    11: "K",
+    12: "L",
+    13: "M",
+    14: "N",
+    15: "O",
+    16: "P",
+    17: "Q",
+    18: "R",
+    19: "S",
+    20: "T",
+    21: "U",
+    22: "V",
+    23: "W",
+    24: "X",
+    25: "Y",
+    26: "Z"
+}
+
+secret = ""
+for x in open("text_files/secret.txt", "r"):
+    x = int(x)
+    secret = secret + dictionary[x]
+print(secret)
 
 
 
